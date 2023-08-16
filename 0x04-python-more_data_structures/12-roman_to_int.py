2b4a158017674e9ac1d66435f2e8d4472f27f486
root <am8122004@gmail.com>
2023-08-16 13:24:49 -0700

    a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and type(roman_string) is str:
-        res = 0
-        for i in reversed(roman_string):
-            if i == 'I':
-                if res >= 5:
-                    res -= 1
-                else:
-                    res += 1
-            elif i == 'V':
-                if res >= 10:
-                    res -= 5
-                else:
-                    res += 5
-            elif i == 'X':
-                if res >= 50:
-                    res -= 50
-                else:
-                    res += 10
-            elif i == 'L':
-                if res >= 100:
-                    res -= 50
-                else:
-                    res += 50
-            elif i == 'C':
-                if res >= 500:
-                    res -= 100
-                else:
-                    res += 100
-            elif i == 'D':
-                if res >= 1000:
-                    res -= 500
-                else:
-                    res += 500
-            elif i == 'M':
-                res += 1000
-            else:
-                return 0
-        return res
-    else:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
+    res = 0
+    for i in reversed(roman_string):
+        if i == 'I':
+            if res >= 5:
+                res -= 1
+            else:
+                res += 1
+        elif i == 'V':
+            if res >= 10:
+                res -= 5
+            else:
+                res += 5
+        elif i == 'X':
+            if res >= 50:
+                res -= 50
+            else:
+                res += 10
+        elif i == 'L':
+            if res >= 100:
+                res -= 50
+            else:
+                res += 50
+        elif i == 'C':
+            if res >= 500:
+                res -= 100
+            else:
+                res += 100
+        elif i == 'D':
+            if res >= 1000:
+                res -= 500
+            else:
+                res += 500
+        elif i == 'M':
+            res += 1000
+        else:
+            return 0
+    return res

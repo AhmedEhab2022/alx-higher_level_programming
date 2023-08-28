a07be10253918ffe1e7a246fd5fe92db17288495
root <am8122004@gmail.com>
2023-08-28 06:17:51 -0700

    a function that prints an integer and prints in stderr the error precede by Exception: .

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
-    msg = "Exception: Unknown format code 'd' for object of type 'str'"
     try:
         print("{:d}".format(value))
     except Exception as exc:
-        print(msg)
+        print("Exception:", exc, file=sys.stderr)
         return False
 
     return True

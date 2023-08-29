1fca259257172da1a5eddf6ed4af44a67668bb9d
root <am8122004@gmail.com>
2023-08-29 08:57:06 -0700

    a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,6 +57,9 @@ class Square:
     def my_print(self):
         """prints in stdout the square with the character #"""
 
+        if self.__size == 0:
+            print()
+            return
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
@@ -64,8 +67,6 @@ class Square:
                 print(' ', end='')
             for j in range(self.__size):
                 print("#", end='')
-            print()
-        if self.__size == 0:
             print()
 
     @property

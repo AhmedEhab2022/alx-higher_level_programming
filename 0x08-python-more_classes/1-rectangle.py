1978e396e35d98a33b46867935307f17f920eee5
root <am8122004@gmail.com>
2023-09-04 04:40:29 -0700

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""Module class define a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle
+
+    Attributes:
+        __width: Private instance width of the rectangle
+        __height: Private instance height of the rectangle
+
+    Args:
+        width: the width of the rectangle
+        height: the height of thr rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """gets the width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width"""
+
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets the height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value

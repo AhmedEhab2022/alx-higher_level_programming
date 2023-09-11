00a03a81e7f08b27b690ace9d6b441a347c75dae
root <am8122004@gmail.com>
2023-09-11 08:50:11 -0700

    a class BaseGeometry (based on 6-base_geometry.py) and the class doctest

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """base class for Geometric shapes"""
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates value ``value``"""
+
+        if type(value) is not int:
+            raise TypeError('<name> must be an integer')
+
+        if value <= 0:
+            raise ValueError('<name> must be greater than 0')

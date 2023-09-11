6a1d55a3c8f3c6fdbccc85aa875d952e93adebc3
root <am8122004@gmail.com>
2023-09-11 12:53:16 -0700

    a class MyInt that inherits from int: MyInt is a rebel. MyInt has == and != operators inverted

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a class MyInt"""
+
+
+class MyInt(int):
+    """A class that inherted from int"""
+
+    def __eq__(self, other):
+        """Inverts the == operator"""
+
+        return not(int(self) == other)
+
+    def __ne__(self, other):
+        """Inverts the != operator"""
+
+        return not(int(self) != other)

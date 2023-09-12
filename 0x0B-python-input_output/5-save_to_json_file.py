9d4c8037a43c612445c5336ade08919c0361ac1b
root <am8122004@gmail.com>
2023-09-12 06:15:15 -0700

    a function that writes an Object to a text file, using a JSON representation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
 
-    json_repr = json.dumps(my_obj)
     with open(filename, "w") as f:
-        f.write(json_repr)
+        f.write(json.dumps(my_obj))

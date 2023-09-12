ce4aa0aed26868f2accb49d733223e538dd56fd8
root <am8122004@gmail.com>
2023-09-12 05:00:36 -0700

    a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,8 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
 
+    if type(filename) != str:
+        raise TypeError('filename must be string')
+
     with open(filename, encoding='utf-8') as f:
         print(f.read())

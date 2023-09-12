b2833e3be8302f870401c1cf4c1a44b05227d9fc
root <am8122004@gmail.com>
2023-09-12 08:21:15 -0700

    a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Define a class Student"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+
+        if type(attrs) is list:
+            my_dict = {}
+            for ele in attrs:
+                if type(ele) is str:
+                    for key in self.__dict__.keys():
+                        if key == ele:
+                            my_dict[key] = self.__dict__[key]
+            return my_dict
+
+        return self.__dict__

992616782fa923da534c3742791c7e695e510a5a
root <am8122004@gmail.com>
2023-09-12 06:08:55 -0700

    writes an Object to a text file, using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Deine a function save_to_json_file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation"""
+
+    if type(filename) is not str:
+        raise TypeError('filename must be string')
+
+    json_repr = json.dumps(my_obj)
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write(json_repr)

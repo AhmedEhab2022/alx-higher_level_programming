0a88f5a1c9c9c2f05a92c07fea057a344ad7a343
root <am8122004@gmail.com>
2023-09-20 07:25:53 -0700

    add draw method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ Module contain ``Base`` Class
 
 import json
 import csv
+import turtle
 
 
 class Base:
@@ -141,3 +142,38 @@ class Base:
                 return list_insts
         except FileNotFoundError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """opens a window and draws all the Rectangles and Squares"""
+
+        pen = turtle.Turtle()
+        pen.pensize(2)
+        pen.hideturtle()
+        screen = turtle.Screen()
+        screen.bgcolor("black")
+
+        if list_rectangles is not None and list_rectangles != []:
+            pen.color("red")
+            pen.fillcolor("red")
+            for rect in list_rectangles:
+                pen.up()
+                pen.goto(rect.x, rect.y)
+                pen.down()
+                for i in range(2):
+                    pen.forward(rect.width)
+                    pen.left(90)
+                    pen.forward(rect.height)
+                    pen.left(90)
+
+        if list_squares is not None and list_squares != []:
+            pen.color("blue")
+            for sq in list_squares:
+                pen.up()
+                pen.goto(sq.x, sq.y)
+                pen.down()
+                for j in range(4):
+                    pen.forward(sq.width)
+                    pen.left(90)
+
+        screen.mainloop()

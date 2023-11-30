981287033ebae79e3fd72979eea11cb7d5d4713c
root <am8122004@gmail.com>
2023-11-30 10:29:54 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,11 +7,5 @@ def find_peak(list_of_integers):
 
     if list_of_integers:
         size = len(list_of_integers)
-        listInt = list_of_integers
-        for i in range(size):
-            if i == 0 and listInt[i] >= listInt[i + 1]:
-                return listInt[i]
-            elif i == size - 1 and listInt[i] > listInt[i - 1]:
-                return listInt[i]
-            elif listInt[i] >= listInt[i - 1] and listInt[i] >= listInt[i + 1]:
-                return listInt[i]
+        listInt = list_of_integers.copy()
+        return sorted(listInt)[size - 1]

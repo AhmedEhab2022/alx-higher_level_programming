26d405fbf818026b6d7f3648723e227281ffd683
root <am8122004@gmail.com>
2023-12-01 09:49:27 -0800

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    request = requests.post(url, data={'email': email})
-    response = requests.get(url, params={'email': email})
-    print(f'Your email is:{response.text}')
+    response = requests.post(url, data={'email': email})
+    print(response.text)

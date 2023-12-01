6d663c504227cb732917bc78904d4f714818dada
root <am8122004@gmail.com>
2023-12-01 13:42:28 -0800

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,18 +9,17 @@ import requests
 
 if __name__ == "__main__":
     letter = sys.argv[1]
+    if !letter:
+        letter = ''
     url = 'http://0.0.0.0:5000/search_user'
-    if letter:
-        value = {'q': letter}
-        response = requests.post(url, data=value)
-        if response.headers.get('Content-Type') != 'application/json':
-            print('Not a valid JSON')
-        elif response.text and len(response.text) == 0:
-            print('No result')
-        else:
-            data_json = response.json()
-            id = data_json.get('id')
-            name = data_json.get('name')
-            print(f'[{id}] {name}')
-    else:
+    value = {'q': letter}
+    response = requests.post(url, data=value)
+    if response.headers.get('Content-Type') != 'application/json':
+        print('Not a valid JSON')
+    elif response.text and len(response.text) == 0:
         print('No result')
+    else:
+        data_json = response.json()
+        id = data_json.get('id')
+        name = data_json.get('name')
+        print(f'[{id}] {name}')

7e3398693ca31ae8200f873e8a58f43599c78660
root <am8122004@gmail.com>
2023-12-01 08:22:54 -0800

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,9 +12,8 @@ import urllib.parse
 if __name__ == "__main__":
     url, email = sys.argv[1], sys.argv[2]
     values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
+    data = urllib.parse.urlencode(values).encode('ascii')
     request = urllib.request.Request(url, data)
-    with urllib.request(url, data) as response:
+    with urllib.request.urlopen(url, data) as response:
         body = response.read()
         print(body.decode('utf-8'))

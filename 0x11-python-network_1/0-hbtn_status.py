9c1374f9003f1bee5768fd324f406bf722219c7b
root <am8122004@gmail.com>
2023-12-01 06:45:35 -0800

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        data = res.read()
+        print("Body response:")
+        print('    - type: {}'.format(type(data)))
+        print('    - content: {}'.format(data))
+        print('    - utf8 content: {}'.format(data.decode('ascii')))

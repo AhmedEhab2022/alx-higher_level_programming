9939ef8a6d5ff9c1d0e58bcb3252a04bea1b5d28
AhmedEhab2022 <am8122004@gmail.com>
2024-02-09 09:57:01 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-filename = process.argv[1];
-const file = require("fs");
-const content = file.readFile(filename, "utf-8", (err, content) => {
+const filename = process.argv[2];
+const file = require('fs');
+file.readFile(filename, 'utf-8', (err, content) => {
   if (err) {
     console.log(err);
   } else {
-	console.log(content);
+    console.log(content);
   }
 });
